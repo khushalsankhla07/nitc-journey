a57fa3ab91a42397b0b5df11e89a3f89c68184ab
Khushal Sankhla <khushalsankhla07@gmail.com>
2026-08-27 15:17:59 +0530

    Day 1: variables, datatypes, input/output

---
+++ day1_variables.py
@@ -0,0 +1,16 @@
+a = 1
+b = 3
+e = 69.70911
+ghoda = 10
+panchi = 7
+c = "gadhuz"
+print(a+b)
+print(c)
+print("the type of c is ", type(c))
+print("the type of a is ", type(a))
+list1 = panchi , ghoda
+print(list1)
+list2 = "panchi" , "ghoda"
+tuple1 = ("gadhu" , "mongose")
+print(tuple1)
+print("the type of e is" , type(e))
