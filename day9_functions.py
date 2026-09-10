ace741b2ace7a4ed3a90758174f6a5fb3232fa7a
Khushal Sankhla <khushalsankhla07@gmail.com>
2026-09-10 22:27:55 +0530

    day9_functions.py

---
+++ day9_functions.py
@@ -0,0 +1,13 @@
+def hello_function():
+    print("Hello Function!")
+n = int(input())
+for i in range(n):
+    print("Hello Function!")
+
+
+
+def cube_number(n):
+    return n*n*n
+n = int(input())
+result = cube_number(n)
+print(result)
